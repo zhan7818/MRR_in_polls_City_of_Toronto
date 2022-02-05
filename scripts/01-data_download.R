d33632b3f75f81149d391ab298f2e1670612e4c1
Tianyi Zhang <tianyi.zhang6546@gmail.com>
2022-02-05 16:11:36 -0500

    Modified 01-data_cleaning.R into 01-data_download.R and added code for downloading the data in a reproducible way. the dataset is stored in a csv file in inputs/data/polls_conducted_by_city and included in this commit.

---
+++ scripts/01-data_download.R
@@ -0,0 +1,38 @@
+#### Preamble ####
+# Purpose: Download data from opendatatoronto
+# Author: Tian Yi Zhang
+# Data: 5 February 2022
+# Contact: psyminette.zhang@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+library(tidyverse)
+library(opendatatoronto)
+
+#### Data download ####
+# Data is from https://open.toronto.ca/dataset/polls-conducted-by-the-city/
+
+# Datasets are grouped into packages that have multiple datasets
+# which are also referred to as 'resources
+# that are relevant to that topic. We obtain the package for the
+# dataset using a unique key obtained from the datasets webpapge
+# (see above).
+
+# Get data
+# First line gets all resources for this package using the 
+# associated unique key.
+# Second line filters for the resource stored in csv format.
+# Third line, get_resource(), will load the first resource by default.
+# Since we have filtered to only one resource, it will load the desired one.
+# If there is more than one resource then we need to add 
+# filter(row_number()==1).
+polls_conducted_by_city <-
+  list_package_resources("7bce9bf4-be5c-4261-af01-abfbc3510309") %>%
+  filter(tolower(format) %in% c('csv', 'geojson')) %>%
+  get_resource()
+         
+#### Save data ####
+write_csv(polls_conducted_by_city, "inputs/data/polls_conducted_by_city.csv")
+
+
+         
